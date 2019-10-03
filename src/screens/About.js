8416ec088595e8b9d211e2c08f3326a31711c16f
Andrew Searcy <andrewsearcy@Andrews-Mac-Pro.local>
2019-10-03 14:16:43 -0500

    added effects

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,6 +2,13 @@ import React from "react";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import PortfolioLogo from "../Logo/Portfolio Logo.png";
+import Responsive from "../images/Responsive.png";
+import Fast from "../images/Fast.png";
+import Creative from "../images/Creative.png";
+import Team from '../images/Team.png';
+import Fade from "react-reveal/Fade";
+import Flip from "react-reveal/Flip";
+
 
 const About = () => (
   <div>
@@ -17,8 +24,50 @@ const About = () => (
         </div>
       </div>
       <div className="sec-2">
+        <Fade left>
         <h1 className="title">ABOUT</h1>
-        <div id="line" />
+        </Fade>
+        <Fade right>
+        <div id="line2" />
+        </Fade>
+        <div className="sec-2-about">
+          <Fade delay={200}>
+          <div className="sec-2-col-about">
+            <a className="about-block">
+              <img className="about-block-bg" src={Responsive} />
+              <h2 className="textUnder">Responsive</h2>
+              <p className='about-text'>My designs will work on any device, big or small.</p>
+            </a>
+          </div>
+          </Fade>
+          <Fade delay={400}>
+          <div className="sec-2-col-about">
+            <a className="about-block">
+              <img className="about-block-bg" src={Creative} />
+              <h2 className="textUnder">Creative</h2>
+              <p className='about-text'>My creativity grabs the users attention. A priority.</p>
+            </a>
+          </div>
+          </Fade>
+          <Fade delay={600}>
+          <div className="sec-2-col-about">
+            <a className="about-block">
+              <img className="about-block-bg" src={Fast} />
+              <h2 className="textUnder">Fast</h2>
+              <p className='about-text'>My code is always written with fast load time in mind.</p>
+            </a>
+          </div>
+          </Fade>
+          <Fade delay={800}>
+          <div className="sec-2-col-about">
+            <a className="about-block">
+              <img className="about-block-bg" src={Team} />
+              <h2 className="textUnder">Team</h2>
+              <p className='about-text'>I enjoy collaberating with others to make a project come to life.</p>
+            </a>
+          </div>
+          </Fade>
+        </div>
       </div>
     </div>
     <Footer></Footer>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,6 +7,7 @@ import PortfolioLogo from "../Logo/Portfolio Logo.png";
 import IMG1 from "../images/IMG1.jpg";
 import IMG2 from "../images/IMG2.jpg";
 import IMG3 from "../images/IMG3.jpg";
+import Fade from "react-reveal/Fade";
 
 const Home = () => (
   <div>
@@ -22,45 +23,77 @@ const Home = () => (
             </Link>
           </div>
         </div>
-        <div className="landing-mid">
-          <div className="landing-row-1">
-            <h1 className="home-title">
-              My name is <span className="name">Andrew Searcy</span>, I love
-              developement & design.
-            </h1>
-            <a className="scroll-icon">
-              <img className="bounce" src={WebsiteArrow} />
-            </a>
+          <div className="landing-mid">
+            <div className="landing-row-1">
+              <Fade top>
+                <h1 className="home-title">
+                  My name is <span className="name">Andrew Searcy</span> and I
+                  love software developement.
+                </h1>
+              </Fade>
+              <Fade bottom>
+                <a className="scroll-icon">
+                  <img className="bounce" src={WebsiteArrow} />
+                </a>
+              </Fade>
+            </div>
           </div>
-        </div>
       </div>
       <div className="sec-2">
         <div className="sec-2-content">
-          <h1 className="title">WORK</h1>
-          <div id="line" />
-          <p className="workBlurb">
-            As a designer and developer based in Tennessee, I believe in
-            combining purposeful graphic design and web developement to create
-            an unforgetable experience for every user.
-          </p>
+          <Fade left>
+            <h1 className="title">WORK</h1>
+          </Fade>
+          <Fade right>
+            <div id="line" />
+          </Fade>
+          <Fade left>
+            <p className="workBlurb">
+              As a designer and developer based in Tennessee, I believe in
+              combining purposeful graphic design and web developement to create
+              an unforgetable experience for every user.
+            </p>
+          </Fade>
         </div>
         <div className="sec-2-work">
-          <div className="sec-2-col">
-            <a className="work-block">
-              <img className="work-block-bg" src={IMG1} />
+          <Fade delay={200}>
+            <div className="sec-2-col">
+              <a className="work-block">
+                <img className="work-block-bg" src={IMG1} />
+              </a>
+            </div>
+          </Fade>
+          <Fade delay={400}>
+            <div className="sec-2-col">
+              <a className="work-block">
+                <img className="work-block-bg" src={IMG2} />
+              </a>
+            </div>
+          </Fade>
+          <Fade delay={600}>
+            <div className="sec-2-col">
+              <a className="work-block">
+                <img className="work-block-bg" src={IMG3} />
+              </a>
+            </div>
+          </Fade>
+        </div>
+      </div>
+      <div className="contact">
+        <Fade left>
+          <div className="contact-col1">
+            <h3 className="contact-col-c looking">
+              Looking for a Software Developer?
+            </h3>
+          </div>
+        </Fade>
+        <Fade right>
+          <div className="contact-col1">
+            <a href="" className="contact-col-c">
+              <h1 className="contact-me">Contact Me</h1>
             </a>
           </div>
-          <div className="sec-2-col">
-            <a className="work-block">
-              <img className="work-block-bg" src={IMG2} />
-            </a>
-            </div>
-            <div className="sec-2-col">
-            <a className="work-block">
-              <img className="work-block-bg" src={IMG3} />
-            </a>
-            </div>
-        </div>
+        </Fade>
       </div>
     </div>
     <Footer />
